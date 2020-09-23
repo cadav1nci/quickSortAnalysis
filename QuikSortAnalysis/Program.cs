1da96f10a6a1a34a036a8e36f17d07139dd47bb7
jairoRdgz <jairitox2000@gmail.com>
2020-09-22 21:45:53 -0500

    Algorithms Implemented
    The two versions of the quicksort are implemented in the main class

--- QuikSortAnalysis/Program.cs
+++ QuikSortAnalysis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,42 @@ namespace QuikSortAnalysis
     {
         static void Main(string[] args)
         {
+        }
+
+        public static void quick_sort(int[] arr, int p, int r)
+        {
+            int j;
+            if (p < r)
+            {
+                j = partition(arr, p, r);
+                quick_sort(arr, p, j - 1);
+                quick_sort(arr, j + 1, r);
+            }
+        }
+
+        public static int partition(int[] A, int p, int r)
+        {
+            int x = A[r];
+            int i = p - 1;
+
+            for (int j = p; j < r-1; j++)
+            {
+                if (A[j]<= x)
+                {
+                    i += 1;
+
+                    int aux = A[i];
+                    A[i] = A[j];
+                    A[j] = aux;
+                }
+            }
+            return i+1;
+        }
+
+        public static int Rand_Parti(int[] A, int p, int r)
+        {
+            int i = Random(p, r);
+            return 0;
         }
     }
 }
